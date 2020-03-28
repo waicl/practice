9fd8782767508707ea13e94c2fa26e30e6986b91
waicl <1091924611@qq.com>
2020-03-28 15:49:35 +0800

    更改了之后的代码

--- suite.py
+++ suite.py
@@ -12,12 +12,14 @@ from utils import BASEDIR
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(LoginSuccess))
 
-filename = BASEDIR + '/report/waicl{}.html'.format(time.strftime('%Y%m%d%H%M%S'))
+# filename = BASEDIR + '/report/waicl{}.html'.format(time.strftime('%Y%m%d%H%M%S'))
+filename = BASEDIR + '/report/waicl.html'
 
 with open(filename,'wb')as d:
     run = HTMLTestRunner(d,verbosity=2,title='ihrm标题',description='ihrm内容')
     run.run(suite)
 
+print('更改输出语句,然后查看是否会自动触发轮循构建，最长不会超过一分钟')
 
 
 
